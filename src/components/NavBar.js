fc7fb84a4b6647b7f328c143aa2080b549fa3b8a
ckcnair <ckcnair@gmail.com>
2021-06-24 19:17:18 +0530

    replacing inline style with makestyle hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,24 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: "#d71a62",
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 const NavBar = () => {
+  const classes = useStyles();
   return (
     <div>
-      <AppBar position="fixed" style={{ background: "#d71a62" }}>
+      <AppBar position="fixed" className={classes.root}>
         <ToolBar>
-          <Typography
-            variant="h2"
-            color="inherit"
-            style={{
-              fontWeight: "bold",
-            }}
-          >
-            shift
-          </Typography>
+          <Typography className={classes.title}>shift</Typography>
         </ToolBar>
       </AppBar>
     </div>
