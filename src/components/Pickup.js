57d23ada256a936bca254dc4c771f8d179f0e8f0
ckcnair <ckcnair@gmail.com>
2021-06-25 00:18:34 +0530

    Adding paper component

--- src/components/Pickup.js
+++ src/components/Pickup.js
@@ -2,14 +2,19 @@ import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "./Card";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     padding: theme.spacing(12, 4),
   },
+  paper: {
+    borderRadius: 20,
+  },
 }));
 const Pickup = () => {
   const classes = useStyles();
+  let counter = 10;
   return (
     <Container component="section" maxWidth="lg" className={classes.root}>
       <Grid container spacing="3" alignItems="center" justify="center">
@@ -17,7 +22,9 @@ const Pickup = () => {
           <Grid container justify="center" spacing="3">
             {[0, 1, 2, 4, 5, 6, 7, 8, 9].map((value) => (
               <Grid key={value} item>
-                <Card />
+                <Paper elevation={counter - value} className={classes.paper}>
+                  <Card />
+                </Paper>
               </Grid>
             ))}
           </Grid>
