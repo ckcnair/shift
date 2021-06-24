d1a3bd663340536972386fbba09e4199c8ead6bf
ckcnair <ckcnair@gmail.com>
2021-06-25 00:17:34 +0530

    pickup form component for modifying order

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import Pickup from "./components/Pickup";
 import DateCard from "./components/DateCard";
-
+import PickupForm from "./components/PickupForm";
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <PickupForm />
       <Pickup />
     </div>
   );
