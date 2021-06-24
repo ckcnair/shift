746a33949edb8d73473aab0cfcaa5e4a7e8c5370
ckcnair <ckcnair@gmail.com>
2021-06-25 05:17:41 +0530

    features of shifting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Pickup from "./components/Pickup";
-import DateCard from "./components/DateCard";
+import Features from "./components/Features";
 import PickupForm from "./components/PickupForm";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <PickupForm />
+      <Features />
       <Pickup />
     </div>
   );
