86cc2c9e64d09cf2e9bb0cb6e0dc939ead6744ad
ckcnair <ckcnair@gmail.com>
2021-06-24 20:34:26 +0530

    pickup dates component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Pickup from "./components/Pickup";
+import DateCard from "./components/DateCard";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Pickup />
     </div>
   );
 }
