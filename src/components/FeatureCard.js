1321be57799ae7ae7c246a256746235cc87657d4
ckcnair <ckcnair@gmail.com>
2021-06-26 00:36:40 +0530

    new class added for background color and padding, modified the width and font sizes for better look

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -12,44 +12,50 @@ const useStyles = makeStyles({
     maxWidth: 100,
     backgroundColor: "#f9fafc",
     objectFit: "none",
-    width: "20%",
+    width: "12%",
   },
   title: {
-    fontSize: 15,
+    fontSize: 12,
     textAlign: "center",
     fontWeight: "bold",
     color: "#000",
   },
   media: {
-    height: 50,
+    height: 40,
     objectFit: "scale-down",
+  },
+  carddiv: {
+    backgroundColor: "#f9fafc",
+    padding: "10px",
   },
 });
 
 export default function FeatureCard({ image, title }) {
   const classes = useStyles();
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="70"
-          image={image}
-          title="Contemplative Reptile"
-          className={classes.media}
-        />
-        <CardContent>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="h5"
-            className={classes.title}
-          >
-            {title}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <div className={classes.carddiv}>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="70"
+            image={image}
+            title="Contemplative Reptile"
+            className={classes.media}
+          />
+          <CardContent>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="h5"
+              className={classes.title}
+            >
+              {title}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </div>
   );
 }
